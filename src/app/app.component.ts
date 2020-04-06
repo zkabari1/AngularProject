8b8d8e3073bfbd0967a6a8048e49a08a59181d6a
zimple kabariya <zkabari1@binghamton.edu>
2020-04-06 19:39:52 -0400

    added css using styleurl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'mb-app',
-    templateUrl:'./app.component.html'
+    templateUrl:'./app.component.html',
+    styleUrls:['./app.component.css']
 })
 
 export class AppComponent{} 
