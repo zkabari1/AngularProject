ff85362b5304136f601bfd7f94a7d08935ac4b06
zimple kabariya <zkabari1@binghamton.edu>
2020-04-06 18:46:59 -0400

    moved template to templateurl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector:'app-root',
-    template:'<h1> My App</h1>'
+    selector:'mb-app',
+    templateUrl:'./app.component.html'
 })
 
 export class AppComponent{} 
