6e72a57f53c4af9f39b2e2c89451f92db8c4f00c
zimple kabariya <zkabari1@binghamton.edu>
2020-04-06 20:18:04 -0400

    added blog item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { BlogItemComponent} from './blog-item.componen';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogItemComponent
   ],
   bootstrap: [
     AppComponent
---
+++ src/app/blog-item.componen.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'mb-blog-item',
+    templateUrl:'./blog-item.component.html',
+    styleUrls:['./blog-item.component.css']
+})
+
+export class BlogItemComponent{} 
