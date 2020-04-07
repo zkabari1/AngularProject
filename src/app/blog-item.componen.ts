69ac3b44312ad706afe32cbfaa47382f5b4f8906
zimple kabariya <zkabari1@binghamton.edu>
2020-04-06 20:36:12 -0400

    interpolation and property binding

--- src/app/blog-item.componen.ts
+++ src/app/blog-item.componen.ts
@@ -6,4 +6,9 @@ import {Component} from '@angular/core';
     styleUrls:['./blog-item.component.css']
 })
 
-export class BlogItemComponent{} 
+export class BlogItemComponent{
+    title="If you loved The Silent Patient, check out these thrillers";
+    date="April 06, 2020";
+    content="What did you love about The Silent Patient? For me, it was the 'relationship' between criminal psychotherapist Theo Faber and his silent patient. Psychotherapists, psychologists, and psychiatrists: there's just something about them that adds that something extra to a thriller for me. Perhaps it's because they aren't as naturally cynical or suspicious as detectives. They're like a stand-in for the reader, willing to follow their patient down the rabbit hole to find the killer, and maybe apt to be taken in by a criminal mastermind. Or in the case of one of the titles on this list, they may even be the criminal mastermind. If you read and loved The Silent Patient, here are a few psychological thrillers you may enjoy.";
+    author="Vannessa Cronin"
+} 
