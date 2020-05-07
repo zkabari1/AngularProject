452205054819ee4a02aa389bcf48e1e93f8b7c20
zimple kabariya <zkabari1@binghamton.edu>
2020-05-07 16:09:01 -0400

    final changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { BlogItemComponent} from './blog-item.component';
-import { BlogItemListComponent } from './blog-item-list.component';
-import {FavoriteDirective} from './favorite.directive';
-import {CategoryListPipe} from './category.list.pipe';
-//import {BlogItemService} from './blog-item.service';
-import {lookupListToken,lookupList} from './providers';
-import {HttpClientModule, HttpXhrBackend} from '@angular/common/http';
-import {MockXHRBackend} from './mock-xhr.backend';
-import { routing } from './app.routing';
-import { CategoryListComponent } from './category-list.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { BlogItemComponent } from "./blog-item.component";
+import { BlogItemListComponent } from "./blog-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
+import { CategoryListPipe } from "./category.list.pipe";
+import { lookupListToken, lookupList } from "./providers";
+import { HttpClientModule, HttpXhrBackend } from "@angular/common/http";
+import { MockXHRBackend } from "./mock-xhr.backend";
+import { routing } from "./app.routing";
+import { CategoryListComponent } from "./category-list.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    routing,
-  ],
+  imports: [BrowserModule, HttpClientModule, routing],
   declarations: [
     AppComponent,
     BlogItemComponent,
     BlogItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    CategoryListComponent
+    CategoryListComponent,
   ],
-  bootstrap: [
-    AppComponent
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: lookupListToken, useValue: lookupList },
+    { provide: HttpXhrBackend, useClass: MockXHRBackend },
   ],
-  providers:[
- //   BlogItemService,
-    {provide:lookupListToken,useValue:lookupList},
-    {provide : HttpXhrBackend,useClass:MockXHRBackend}
-  ]
 })
 export class AppModule {}
